ad64df7f746c9cb8746ea5a6fabe014367f4c290
STEN <sten.li@foxmail.com>
2024-12-05 14:28:50 +0800

    Update ActionPanel.vue

--- src/components/pmtOverride/ActionPanel.vue
+++ src/components/pmtOverride/ActionPanel.vue
@@ -1,20 +1,33 @@
 <template>
   <teleport :to="'.comfyui-body-bottom'">
-    <div
-      id="pmt-action-panel"
-      class="fixed bottom-0 inset-x-0 z-[1000] p-[10px] flex justify-center items-center pointer-events-none"
-    >
-      <ButtonGroup class="flex pointer-events-auto">
+    <Panel id="pmt-action-panel">
+      <ButtonGroup>
         <Button
+          v-if="!running"
           class="btn-run"
           size="small"
           :aria-label="'Run'"
+          :aria-haspopup="true"
+          :aria-controls="'btn-run-menu'"
           icon="pi pi-play-circle"
           severity="secondary"
           :loading="running"
           :disabled="saving"
           @click="run"
+          @contextmenu="!saving && !running && runMenu.toggle($event)"
         />
+        <Button
+          v-else
+          class="btn-run-stop"
+          size="small"
+          :aria-label="'Stop'"
+          icon="pi pi-stop-circle"
+          severity="danger"
+          :loading="false"
+          :disabled="pausing"
+          @click="stop"
+        />
+        <Menu ref="runMenu" id="btn-run-menu" :model="runMenuItems" popup />
         <Button
           class="btn-pip"
           size="small"
@@ -22,6 +35,7 @@
           icon="pi pi-circle-fill"
           severity="secondary"
           :style="{ color: pipeline.color }"
+          :loading="running"
           :disabled="running || saving"
           @click="togglePipOver"
         />
@@ -62,7 +76,7 @@
                 :severity="delBtnHovered ? 'danger' : 'secondary'"
                 :loading="false"
                 :disabled="false"
-                @click="comfirmDelete"
+                @click="confirmDelete"
               />
             </div>
             <div class="flex items-center justify-end mt-3">
@@ -95,7 +109,7 @@
           severity="secondary"
           :loading="saving"
           :disabled="running"
-          @click="save"
+          @click="confirmSave"
         />
         <Button
           class="btn-exp"
@@ -108,14 +122,22 @@
           @click="exportJson"
         />
       </ButtonGroup>
-      <ConfirmDialog dismissable-mask :draggable="false"></ConfirmDialog>
-    </div>
+      <ConfirmDialog
+        group="confirm_deletion"
+        dismissable-mask
+        :draggable="false"
+      />
+      <ConfirmPopup group="confirm_saving" />
+      <Toast />
+    </Panel>
   </teleport>
 </template>
 
 <script setup>
-import { shallowRef, ref, onMounted, onUnmounted } from 'vue'
-import { useElementHover } from '@vueuse/core'
+import { shallowRef, ref, computed, onMounted, onUnmounted } from 'vue'
+import { useBrowserLocation, useElementHover } from '@vueuse/core'
+import Panel from 'primevue/panel'
+import Menu from 'primevue/menu'
 import ButtonGroup from 'primevue/buttongroup'
 import Button from 'primevue/button'
 import Popover from 'primevue/popover'
@@ -123,6 +145,9 @@ import InputGroup from 'primevue/inputgroup'
 import InputGroupAddon from 'primevue/inputgroupaddon'
 import InputText from 'primevue/inputtext'
 import ConfirmDialog from 'primevue/confirmdialog'
+import ConfirmPopup from 'primevue/confirmpopup'
+import Toast from 'primevue/toast'
+import { useToast } from 'primevue/usetoast'
 import { useConfirm } from 'primevue/useconfirm'
 import { LiteGraph } from '@comfyorg/litegraph'
 import { app as comfyApp } from '@/scripts/app'
@@ -158,6 +183,21 @@ const statusColor = {
   done: '#0f0', // green
   error: '#f00' // red
 }
+
+const location = useBrowserLocation()
+const volViewUrl = computed(() => {
+  const search = `?uiMode=lite&layoutName=${'Axial Only'}`
+  let { port, origin, pathname } = location.value
+  if (origin === 'file://') {
+    pathname = pathname.replace('comfyui/', 'volview/')
+  } else {
+    if (port) {
+      origin = origin.replace(port, `${+port - 3}`)
+    }
+    pathname = pathname.replace('comfyui/', '') + 'volview/'
+  }
+  return origin + pathname + search
+})
 
 onMounted(() => {
   Object.keys(SYSTEM_NODE_DEFS).forEach((type) => {
@@ -203,7 +243,16 @@ onMounted(() => {
         //   return _onDrawBackground?.apply(this, arguments)
         // }
 
-        // node.addDOMWidget(...
+        // const div = document.createElement('div')
+        // div.classList.add('relative', 'overflow-hidden')
+        // div.innerHTML = `
+        //   <div class="absolute inset-0 overflow-hidden" style="background: white; color: black;">
+        //     <iframe src="${volViewUrl.value}" frameborder="0" width="100%" height="100%"></iframe>
+        //   </div>
+        // `
+        // const widget = node.addDOMWidget('pmt_volview_embed', 'volview-embed', div, {})
+        // console.log(widget)
+        console.log(volViewUrl.value)
       }
     }
   })
@@ -224,6 +273,37 @@ function onDrop(e) {
   e.preventDefault()
 }
 
+const runMenu = ref()
+const runMenuItems = ref([
+  {
+    label: 'Run',
+    icon: 'pi pi-play',
+    class: 'text-sm',
+    disabled: false,
+    command: () => {
+      run()
+    }
+  },
+  {
+    label: 'Run (step-by-step)',
+    icon: 'pi pi-step-forward',
+    class: 'text-sm',
+    disabled: false,
+    command: () => {
+      run()
+    }
+  },
+  {
+    label: 'Run (to selected)',
+    icon: 'pi pi-fast-forward',
+    class: 'text-sm',
+    disabled: true,
+    command: () => {
+      //
+    }
+  }
+])
+
 const pipOver = ref()
 function togglePipOver(e) {
   initNameAndColor()
@@ -233,9 +313,11 @@ function togglePipOver(e) {
 const delBtn = ref()
 const delBtnHovered = useElementHover(delBtn)
 
+const toast = useToast()
 const confirm = useConfirm()
-const comfirmDelete = (e) => {
+const confirmDelete = (e) => {
   confirm.require({
+    group: 'confirm_deletion',
     header: 'Delete Pipeline',
     message: 'Do you want to delete this pipeline?',
     icon: 'pi pi-exclamation-circle',
@@ -249,15 +331,39 @@ const comfirmDelete = (e) => {
       label: 'Delete',
       severity: 'danger'
     },
-    reject: () => {},
     accept: async () => {
       console.log('deleting...')
       // ...
-    }
+    },
+    reject: () => {}
   })
   togglePipOver(e)
 }
+const confirmSave = (e) => {
+  if (running.value) {
+    return
+  }
+  if (saving.value) {
+    return
+  }
+  confirm.require({
+    target: e.currentTarget,
+    group: 'confirm_saving',
+    message: 'Save changes?',
+    rejectProps: {
+      label: 'Cancel',
+      severity: 'secondary',
+      outlined: true
+    },
+    acceptProps: {
+      label: 'Save'
+    },
+    accept: save,
+    reject: () => {}
+  })
+}
 
+let runningTimer = null
 const running = ref(false)
 async function run() {
   if (running.value) {
@@ -265,7 +371,23 @@ async function run() {
   }
   running.value = true
   // ...
-  await new Promise((r) => setTimeout(r, 1000))
+  await new Promise((r) => {
+    runningTimer = setTimeout(r, 5000)
+  })
+  running.value = false
+}
+
+const pausing = ref(false)
+async function stop() {
+  if (pausing.value || !running.value) {
+    return
+  }
+  pausing.value = true
+  // ...
+  await new Promise((r) => setTimeout(r))
+  pausing.value = false
+  clearTimeout(runningTimer)
+  runningTimer = null
   running.value = false
 }
 
@@ -275,9 +397,16 @@ async function save() {
     return
   }
   saving.value = true
+  console.log('saving...')
   // ...
   await new Promise((r) => setTimeout(r, 1000))
   saving.value = false
+  toast.add({
+    severity: 'success',
+    summary: 'Saved',
+    detail: 'Changes have been saved!',
+    life: 3000
+  })
 }
 
 function exportJson() {
@@ -334,3 +463,31 @@ function getWorkflowJson() {
   return workflow
 }
 </script>
+
+<style>
+#pmt-action-panel {
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 10px;
+  z-index: 1000;
+  overflow: hidden;
+}
+#pmt-action-panel .p-panel-header {
+  display: none;
+}
+#pmt-action-panel .p-panel-content {
+  padding: 0;
+}
+#pmt-action-panel .p-panel-content .p-buttongroup {
+  display: flex;
+}
+#pmt-action-panel .p-panel-content .p-buttongroup > .p-button {
+  --p-icon-size: 0.875rem;
+  border-radius: 0 !important;
+}
+
+#btn-run-menu {
+  margin-top: -5px !important;
+}
+</style>
