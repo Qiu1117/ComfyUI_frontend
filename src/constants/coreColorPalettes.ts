72e089312c657febded9b7c901d7c905633b3869
STEN <sten.li@foxmail.com>
2025-01-06 09:44:07 +0800

    Update coreColorPalettes.ts

--- src/constants/coreColorPalettes.ts
+++ src/constants/coreColorPalettes.ts
@@ -4,6 +4,7 @@ import github from '@/assets/palettes/github.json'
 import light from '@/assets/palettes/light.json'
 import nord from '@/assets/palettes/nord.json'
 import solarized from '@/assets/palettes/solarized.json'
+import { DATATYPE_COLOR } from '@/constants/pmtCore'
 import type { ColorPalettes, CompletedPalette } from '@/types/colorPaletteTypes'
 
 export const CORE_COLOR_PALETTES: ColorPalettes = {
@@ -18,3 +19,8 @@ export const CORE_COLOR_PALETTES: ColorPalettes = {
 export const DEFAULT_COLOR_PALETTE: CompletedPalette = dark
 export const DEFAULT_DARK_COLOR_PALETTE: CompletedPalette = dark
 export const DEFAULT_LIGHT_COLOR_PALETTE: CompletedPalette = light
+
+DEFAULT_COLOR_PALETTE.colors.node_slot = {
+  ...DEFAULT_COLOR_PALETTE.colors.node_slot,
+  ...DATATYPE_COLOR
+}
